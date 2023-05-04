ed95bbcf8681d9bc9103ad8ddbcf6344176aadaa
Menard Paul <menardlycee@gmail.com>
2023-05-04 17:12:59 +0200

    contact presque fini

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,9 +3,42 @@ import React from 'react'
 
 export default function contact() {
   return (
-    
-        <div>contact</div>
-    
-    
+    <Layout>
+      <div id='contactpage'>
+        <div id='contact_content'>
+          <h2>Contact Information</h2>
+          <div id='contact_info'>
+            <p>Adress: 198 west 21th treer, Suite 721 New York NY 10016</p>
+            <p>Phone: <span>+ 1235 2355 98</span></p>
+            <p>Email: <span>info@yoursite.com</span></p>
+            <p>Website: <span>yoursite.com</span></p>
+          </div>
+          <div id='contact3'>
+            <div id='contact_form'>
+              <form>
+                <div>
+                  <input type="text" placeholder='Your Name' />
+                </div>
+                <div>
+                  <input type="email" name="email" id="email" placeholder='You Email' />  
+                </div>
+                <div>
+                  <input type="text" placeholder='Subjet of your Message' />
+                </div>
+                <div>
+                  <textarea name="" id="" cols="85" rows="10" placeholder='Message'></textarea>
+                </div>
+                <div>
+                  <button>Send Message</button>
+                </div>
+              </form>
+            </div>
+            <div id='contact_map'>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
   )
 }
